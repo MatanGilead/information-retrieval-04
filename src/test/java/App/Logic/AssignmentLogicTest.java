7a23d96238c83acb5ad6b2cee782ceb3f2c481e2
Or Nachmias <or.nachmias@gmail.com>
2018-06-22 21:19:11 +0300

    Add tests

--- src/test/java/App/Logic/AssignmentLogicTest.java
+++ src/test/java/App/Logic/AssignmentLogicTest.java
@@ -61,11 +61,72 @@ public class AssignmentLogicTest {
         assignmentLogic.train();
 
         // Get Results
-        for (Integer i = 0; i < 1; i++){
+        for (Integer i = 1; i < 50; i++){
             Measurement result = assignmentLogic.run(i);
             System.out.println("K="+i+
                     ",MicroAveraging="+result.getMicroAveraging()+
                     ",MacroAveraging="+result.getMacroAveraging());
         }
+    }
+
+    @Test
+    public void run_testMultipleThreadsValues_checkTime() throws Exception {
+        // Arrange
+        FileDataAccess fileDataAccess = new FileDataAccess();
+        ParameterFileParser parameterFileParser = Mockito.mock((ParameterFileParser.class));
+
+        // Removed from resources since git can't handle it
+        when(parameterFileParser.getTrainFile()).thenReturn("train.csv");
+
+        // Since we run a lot of tests, we don't want all queries
+        when(parameterFileParser.getTestFile())
+                .thenReturn(TestHelper.getFilePathFromResources("TestSet01/test.csv"));
+
+        when(parameterFileParser.getKValue()).thenReturn(20);
+
+        when(parameterFileParser.getOutputFile()).thenReturn("out/output.txt");
+
+        for(Integer i = 1; i < 6; i++){
+            long startTime = System.currentTimeMillis();
+
+            when(parameterFileParser.getThreadsNum()).thenReturn(i);
+            AssignmentLogic assignmentLogic =
+                    new AssignmentLogic(fileDataAccess, parameterFileParser,"dummy");
+
+            // Act
+            assignmentLogic.train();
+
+            // Get Results
+            Measurement result = assignmentLogic.run(42);
+
+            long endTime = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+            System.out.println("NumberOfThreads="+i+",TotalTime="+totalTime);
+        }
+    }
+
+    @Test
+    public void run_entireDataAndTest_returnValidResults() throws Exception {
+        // Arrange
+        FileDataAccess fileDataAccess = new FileDataAccess();
+        ParameterFileParser parameterFileParser = Mockito.mock((ParameterFileParser.class));
+
+        // Removed from resources since git can't handle it
+        when(parameterFileParser.getTrainFile()).thenReturn("train.csv");
+        when(parameterFileParser.getTestFile()).thenReturn("test.csv");
+        when(parameterFileParser.getKValue()).thenReturn(42);
+        when(parameterFileParser.getThreadsNum()).thenReturn(2);
+        when(parameterFileParser.getOutputFile()).thenReturn("out/output.txt");
+
+        AssignmentLogic assignmentLogic =
+                new AssignmentLogic(fileDataAccess, parameterFileParser,"dummy");
+
+        // Act
+        assignmentLogic.train();
+        Measurement result = assignmentLogic.run(null);
+
+        System.out.println("K=42,NumberOfThreads=2"+
+                ",MicroAveraging="+result.getMicroAveraging()+
+                ",MacroAveraging="+result.getMacroAveraging());
     }
 }
